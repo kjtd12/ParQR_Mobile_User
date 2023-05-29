411cf414a0cade02c150f98952b538176487a823
AlgoChad <104708287+AlgoChad@users.noreply.github.com>
2023-05-29 11:28:00 +0800

    some fixes

--- App.js
+++ App.js
@@ -1,15 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import Tabs from './navigation/tabs'
 import LoginStack from './navigation/loginStack';
-import { Screen } from 'react-native-screens';
 
 
 export default function App() {
   return (
     <NavigationContainer>
+      <StatusBar />
       <LoginStack/>
     </NavigationContainer>
   );
