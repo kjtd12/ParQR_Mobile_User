987b03febd07dc1c2f1cac1d2985df9fc47441af
AlgoChad <104708287+AlgoChad@users.noreply.github.com>
2023-05-29 21:55:17 +0800

    Final

--- components/DateModal.js
+++ components/DateModal.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Modal, View, Text, TextInput, TouchableOpacity, Image } from 'react-native';
+import { Modal, View, Text, TextInput, TouchableOpacity, TouchableWithoutFeedback, Image, Platform } from 'react-native';
 import DateTimePicker from '@react-native-community/datetimepicker';
 
 const DateModal = ({ isVisible, onClose, onSubmit }) => {
@@ -7,6 +7,14 @@ const DateModal = ({ isVisible, onClose, onSubmit }) => {
   const [endDate, setEndDate] = useState(null);
   const [showStartDatePicker, setShowStartDatePicker] = useState(false);
   const [showEndDatePicker, setShowEndDatePicker] = useState(false);
+
+  const handleStartDatePress = () => {
+    setShowStartDatePicker(true);
+  };
+
+  const handleEndDatePress = () => {
+    setShowEndDatePicker(true);
+  };
 
   const handleStartDateChange = (event, selectedDate) => {
     const currentDate = selectedDate || startDate;
@@ -22,60 +30,67 @@ const DateModal = ({ isVisible, onClose, onSubmit }) => {
 
   const handleSubmit = () => {
     onSubmit(startDate, endDate);
-    
   };
 
   return (
-  <Modal visible={isVisible} animationType="slide" transparent={true} >
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center', backgroundColor: 'rgba(0,0,0,0.1)', }}>
-      <View style={{ backgroundColor: '#fff', borderRadius: 10, padding: 20, width: '90%' }}>
-        <TouchableOpacity onPress={onClose} style={{ position: 'absolute', top: 10, right: 15, padding: 5 }}>
-          <Image
-            source={ require('../assets/transactionIcons/close.png') }
-            style={{ width: 20, height: 20 }}
-          />
-        </TouchableOpacity>
-        <View style={{ flexDirection: 'row', justifyContent: 'center' }}><Text style={{ fontSize: 20, fontWeight: 'bold', marginBottom: 30, marginTop: 20, color: '#213A5C' }}>Custom Range</Text></View>
-        <Text style={{ paddingVertical: 5, color: '#213A5C', fontWeight: 'bold' }}>From</Text>
-        <TouchableOpacity onPress={() => setShowStartDatePicker(true)} style={{ borderWidth: 1, borderColor: '#213A5C', borderRadius: 5, marginBottom: 20 }}>
-          <TextInput
-            value={startDate? startDate.toLocaleDateString() : ''}
-            placeholder="Select a start date"
-            style={{ padding: 10 }}
-            editable={false}
-          />
-        </TouchableOpacity>
-        {showStartDatePicker && (
-          <DateTimePicker
-            value={startDate || new Date()}
-            mode="date"
-            display="default"
-            onChange={handleStartDateChange}
-          />
-        )}
-        <Text style={{ paddingVertical: 5, color: '#213A5C', fontWeight: 'bold' }}>To</Text>
-        <TouchableOpacity onPress={() => setShowEndDatePicker(true)} style={{ borderWidth: 1, borderColor: '#213A5C', borderRadius: 5, marginBottom: 20 }}>
-          <TextInput
-             value={endDate ? endDate.toLocaleDateString() : ''}
-            placeholder="Select an end date"
-            style={{ padding: 10 }}
-            editable={false}
-          />
-        </TouchableOpacity>
-        {showEndDatePicker && (
-          <DateTimePicker
-            value={endDate || new Date()}
-            mode="date"
-            display="default"
-            onChange={handleEndDateChange}
-          />
-        )}
-        <TouchableOpacity onPress={handleSubmit} style={{ backgroundColor: '#213A5C', borderRadius: 5, padding: 10, marginTop: 30 }}>
-          <Text style={{ color: '#fff', textAlign: 'center', fontWeight: 'bold' }}>Confirm</Text>
-        </TouchableOpacity>
+    <Modal visible={isVisible} animationType="slide" transparent={true}>
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: 'rgba(0,0,0,0.1)' }}>
+        <View style={{ backgroundColor: '#fff', borderRadius: 10, padding: 20, width: '90%' }}>
+          <TouchableOpacity onPress={onClose} style={{ position: 'absolute', top: 10, right: 15, padding: 5 }}>
+            <Image
+              source={require('../assets/transactionIcons/close.png')}
+              style={{ width: 20, height: 20 }}
+            />
+          </TouchableOpacity>
+          <View style={{ flexDirection: 'row', justifyContent: 'center' }}>
+            <Text style={{ fontSize: 20, fontWeight: 'bold', marginBottom: 30, marginTop: 20, color: '#213A5C' }}>
+              Custom Range
+            </Text>
+          </View>
+          <Text style={{ paddingVertical: 5, color: '#213A5C', fontWeight: 'bold' }}>From</Text>
+          <TouchableWithoutFeedback onPress={handleStartDatePress}>
+            <View style={{ borderWidth: 1, borderColor: '#213A5C', borderRadius: 5, marginBottom: 20 }}>
+              <TextInput
+                value={startDate ? startDate.toLocaleDateString() : ''}
+                placeholder="Select a start date"
+                style={{ padding: 10 }}
+                editable={false}
+              />
+            </View>
+          </TouchableWithoutFeedback>
+          {showStartDatePicker && (
+            <DateTimePicker
+              value={startDate || new Date()}
+              mode="date"
+              display="default"
+              onChange={handleStartDateChange}
+            />
+          )}
+          <Text style={{ paddingVertical: 5, color: '#213A5C', fontWeight: 'bold' }}>To</Text>
+          <TouchableWithoutFeedback onPress={handleEndDatePress}>
+            <View style={{ borderWidth: 1, borderColor: '#213A5C', borderRadius: 5, marginBottom: 20 }}>
+              <TextInput
+                value={endDate ? endDate.toLocaleDateString() : ''}
+                placeholder="Select an end date"
+                style={{ padding: 10 }}
+                editable={false}
+              />
+            </View>
+          </TouchableWithoutFeedback>
+          {showEndDatePicker && (
+            <DateTimePicker
+              value={endDate || new Date()}
+              mode="date"
+              display="default"
+              onChange={handleEndDateChange}
+            />
+          )}
+          <TouchableOpacity onPress={handleSubmit} style={{ backgroundColor: '#213A5C', borderRadius: 5, padding: 10, marginTop: 30 }}>
+            <Text style={{ color: '#fff', textAlign: 'center', fontWeight: 'bold' }}>Confirm</Text>
+          </TouchableOpacity>
+        </View>
       </View>
-    </View>
-  </Modal>
+    </Modal>
   );
 };
 
