c9531843c40153fe956a1bdd028f5b7df46a2bbc
AlgoChad <104708287+AlgoChad@users.noreply.github.com>
2023-06-04 12:28:02 +0800

    Final

--- components/DetailsModal.js
+++ components/DetailsModal.js
@@ -58,7 +58,7 @@ const DetailsModal = ({ isVisible, onClose, item }) => {
                         <Text style={styles.secondText}>{durationText}</Text>
                     </View>
                     <View style={styles.detailLine}>
-                        <Text style={styles.firstText}>Discount</Text>
+                        <Text style={styles.firstText}>Discounts</Text>
                         <Text style={styles.secondText}>{item.discount}</Text>
                     </View>
                     <View style={styles.detailLine}>
